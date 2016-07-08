78ca1d3a0dc2286fce66e83035400a401f9a4cd1
amogh bihani <amogh1001@gmail.com>
2016-07-09 02:35:17 +0530

    Posing response to server

---
+++ frontend/scripts/httpRequest.js
@@ -0,0 +1,73 @@
+function httpRequestGet(url, header, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  httpRequest(xhr, header, "", callback);
+}
+
+function httpRequestPost(url, header, body, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  httpRequest(xhr, header, body, callback);
+}
+
+function httpRequestPut(url, header, body, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('PUT', url, true);
+  httpRequest(xhr, header, body, callback);
+}
+
+function httpRequestDelete(url, header, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('DELETE', url, true);
+  httpRequest(xhr, header, "", callback);
+}
+
+function httpRequest(xhr, header, body, callback) {
+  showLoadingSpinner(true);
+  for (var i = 0; i < header.length; ++i) {
+    xhr.setRequestHeader(header[i].type, header[i].value);
+  }
+  xhr.onload = function() {
+    showLoadingSpinner(false);
+    if (parseInt(xhr.status / 10) == 20) {
+      console.log("http request successful");
+      callback(xhr.response);
+    } else {
+      console.log("error receiving response" + xhr.response);
+    }
+  }
+  xhr.send(body);
+}
+
+var mLoadingSpinner;
+function showLoadingSpinner(show) {
+  console.log(show);
+  if (mLoadingSpinner == undefined) {
+    createLoadingSpinner();
+  }
+  try {
+    if (show) {
+      document.body.appendChild(mLoadingSpinner);
+    } else {
+      document.body.removeChild(mLoadingSpinner);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createLoadingSpinner() {
+  var loadingImage = document.createElement("img");
+  loadingImage.src = "res/loading.gif";
+  loadingImage.style.width = "100%";
+
+  mLoadingSpinner = document.createElement("div");
+  mLoadingSpinner.style.background = "#d32f2f";
+  mLoadingSpinner.style.borderRadius = "4px";
+  mLoadingSpinner.style.width = "10%";
+  mLoadingSpinner.style.padding = "3%";
+  mLoadingSpinner.style.position = "fixed";
+  mLoadingSpinner.style.top = "42%";
+  mLoadingSpinner.style.left = "42%";
+  mLoadingSpinner.appendChild(loadingImage);
+}
